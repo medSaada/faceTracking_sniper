2c2b20830d0e0ad8c9807d85c6a0d56e84c1dae1
Med Saada <aminousaada@gmail.com>
2023-10-30 15:12:27 +0100

    it ttracking the page and displaying the PID contoller output

---
+++ main.py
@@ -0,0 +1,54 @@
+import cv2
+from utilities import *
+import matplotlib.pyplot as plt
+from collections import deque
+
+
+
+w=340
+h=240
+#PID CONCTANTS VALUES
+PID=[0.5,0,0.5]
+speed_values = deque(maxlen=100)  # Adjust the maximum number of values to display on the plot
+# plt.figure(figsize=(8, 6))
+# plt.title('Speed Output Variation')
+# plt.xlabel('Time')
+# plt.ylabel('Speed')
+# plt.ylim(-100, 100)  # Set y-axis limit to display speed range (-100 to 100)
+# plt.grid()
+plt.style.use('dark_background')
+
+errorPidPrecedent=0
+cap = cv2.VideoCapture(0)
+fig, (ax1, ax2) = plt.subplots(2, 1)
+
+if not cap.isOpened():
+    print("Error: Could not access the webcam")
+else:
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, (w, h))
+        frame,infos=FaceDetection(frame)
+        errorPidPrecedent,speed = faceTracking(infos, PID, w, h, errorPidPrecedent)
+        speed_values.append(speed)
+
+        # Clear the plot and plot the updated speed values
+        ax2.clear()
+        ax2.plot(speed_values, color='red', linestyle='-', linewidth=1.5)  # Red curve
+        ax2.set_title('Speed Output Variation', color='white', fontsize=14,fontweight='bold')  # Title with larger font and bold
+        ax2.set_xlabel('Time', color='white', fontsize=10)  # X-axis label with larger font
+        ax2.set_ylabel('Speed', color='white', fontsize=10)  # Y-axis label with larger font
+        ax2.set_ylim(-100, 100)
+        ax2.grid(True, color='gray')
+
+        # Display the plot
+        ax1.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        #plt.pause(0.01)
+        #cv2.imshow('Tracker', frame)
+        plt.pause(0.01)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ utilities.py
@@ -0,0 +1,59 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+def FaceDetection(img):
+    imgGrayScale=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    print('imagegrayscale')
+    faceCascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    print('objectCascadeClassifier done')
+
+    DophinoTechfaces=faceCascade.detectMultiScale(imgGrayScale,1.1,4)
+    print('objectCascadeClassifier done')
+
+    centerOfFace=[]
+    faceArea=[]
+    for (x,y,w,h) in DophinoTechfaces:
+        #it draw only horizental or vertcical rectangles
+        label = "Mohamed Amine SAADA"
+        label_position = (x, y - 10)
+
+        cv2.putText(img, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36, 255, 12), 2)
+
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2,)
+        centerx=x+(w//2)
+        centery=y+h//2
+        area=w*h
+        centerOfFace.append([centerx,centery])
+        faceArea.append(area)
+
+    if len(faceArea)!=0:
+        #index of the face that have the maximal area
+        i=faceArea.index(max(faceArea))
+        return img,[faceArea[i],centerOfFace[i]]
+    else:
+
+        return img,[0,[0,0]]
+
+
+def faceTracking(infos,PID, w, h, errorPidPrecedent):
+
+
+    theTarget = w // 2
+    # calculating the error of The PID controller
+    errorPid = infos[1][0] - theTarget
+    P = PID[0] * errorPid
+    # we don't require a Integrater in our case
+    I = 0
+    D = PID[2] * (errorPid - errorPidPrecedent)
+    speed = P + I + D
+    # clip in numpy make the speed don't go out the interval -100 *** 100 if speed>100 the speed go above 100 it will be fixed as 100 and same
+    speed = np.clip(int(speed), -100, 100)
+    print(f"The speed from the PID controller{infos}")
+
+
+    errorPidPrecedent = errorPid
+    if infos==[0,[0,0]]:
+        speed=0
+
+    return errorPidPrecedent,speed
